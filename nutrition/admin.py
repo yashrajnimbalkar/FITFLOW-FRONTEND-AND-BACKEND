0fe8d1c62f8a0fc34564af0aaf9625d27712cf21
yashrajnimbalkar <yashraj12042005@outlook.com>
2025-03-18 22:55:24 +0530

    Delete nutrition directory

--- nutrition/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import UserInput
-from .models import DietRecommendation
-
-# Register the model
-admin.site.register(UserInput)
-admin.site.register(DietRecommendation)
--- nutrition/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import DietRecommendationAPI
-
-urlpatterns = [
-    path('',DietRecommendationAPI.as_view(), name='diet_recommendation'),
-]
