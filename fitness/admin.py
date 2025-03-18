e097d4e24b786eca3677e44cd2d0e69600b5647c
yashrajnimbalkar <yashraj12042005@outlook.com>
2025-03-18 22:54:46 +0530

    Delete fitness directory

--- fitness/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import FitnessRecommendation, FitnessInput
-
-# Register the model
-admin.site.register(FitnessInput)
-admin.site.register(FitnessRecommendation)
--- fitness/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import FitnessRecommendationAPI, UserFitnessPlansAPI
-
-urlpatterns = [
-    path('fitness-recommendation/', FitnessRecommendationAPI.as_view(), name='fitness-recommendation'),
-    path('user-plans/', UserFitnessPlansAPI.as_view(), name='user-plans'),
-]
