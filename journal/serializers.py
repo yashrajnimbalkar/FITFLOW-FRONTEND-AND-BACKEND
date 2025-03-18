1293f64213663771ae1ab57cf23b414bc0053217
yashrajnimbalkar <yashraj12042005@outlook.com>
2025-03-18 22:54:59 +0530

    Delete journal directory

--- journal/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import JournalEntry
-
-class JournalEntrySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = JournalEntry
-        fields = ['id', 'date', 'mood', 'notes']  # Fields to include in the API
